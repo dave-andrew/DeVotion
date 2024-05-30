4c13356f659a334ab2572cdc87f746505b9a61c2
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-05-30 10:25:10 +0700

    feat: choose workspace type UI

--- resources/views/pages/create-workspace.blade.php
+++ resources/views/pages/create-workspace.blade.php
@@ -1,54 +1,67 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Workspace</title>
-    @vite('resources/css/app.css')
-</head>
-<body class="dark:bg-primary">
-<div class="w-full flex flex-col items-center pt-20">
-    <h3 class="text-xl font-bold dark:text-white">How do you want to use DeVotion?</h3>
-    <h2 class="text-2xl font-bold text-gray-500">This helps customize your experience</h2>
-    <div class="">
-{{--        TODO: belom kelar buat inputannya--}}
-        <form action="{{ route('createWorkspace') }}" method="post" class="flex flex-col justify-center gap-16">
-            @csrf
-            <div class="flex justify-center gap-16 pt-10">
-                <label class="flex flex-col justify-center items-center cursor-pointer">
-                    <input type="radio" name="type" value="personal" class="visually-hidden">
-                    <div class="flex flex-col justify-center items-center cursor-pointer p-4 rounded-xl border-2 border-gray-700">
-                        <img src='{{asset('plan-type-for-work-darkmode.png')}}' alt="Personal Use" class="w-[200px] h-auto" />
-                        <div class="font-medium text-lg dark:text-white pt-4">Personal</div>
-                    </div>
+@extends('layouts.workspace')
 
-                </label>
+@section('title', 'Create Workspace')
 
-                <label class="flex flex-col justify-center items-center cursor-pointer">
-                    <input type="radio" name="type" value="team" class="visually-hidden">
-                    <div class="flex flex-col justify-center items-center cursor-pointer p-4 rounded-xl border-2 border-gray-700">
-                        <img src='{{asset('plan-type-for-life-darkmode.png')}}' alt="Team Use" class="w-[200px] h-auto" />
-                        <div class="font-medium text-lg dark:text-white pt-4">Team</div>
-                    </div>
-                </label>
-            </div>
+@section('content')
+    <div class="w-full flex flex-col items-center pt-20">
+        <h3 class="text-xl font-bold dark:text-white">How do you want to use DeVotion?</h3>
+        <h2 class="text-2xl font-bold text-gray-500">This helps customize your experience</h2>
+        <div class="">
+            {{--        TODO: belom kelar buat inputannya--}}
+            <form action="{{ route('createWorkspace') }}" method="post" class="flex flex-col justify-center gap-16">
+                @csrf
+                <div class="flex justify-center gap-16 pt-10">
+                    <label class="flex flex-col justify-center items-center cursor-pointer">
+                        <input type="radio" name="type" value="personal" class="visually-hidden">
+                        <div class="flex flex-col justify-center items-center cursor-pointer p-4 rounded-xl border-2 border-gray-700">
+                            <img src='{{asset('plan-type-for-work-darkmode.png')}}' alt="Personal Use" class="w-[200px] h-auto" />
+                            <div class="font-medium text-lg dark:text-white pt-4">Personal</div>
+                        </div>
 
-            <button class="bg-blue-500 rounded-lg text-white py-2 mx-[5rem]" type="submit">
-                Continue
-            </button>
+                    </label>
 
-        </form>
+                    <label class="flex flex-col justify-center items-center cursor-pointer">
+                        <input type="radio" name="type" value="team" class="visually-hidden">
+                        <div class="flex flex-col justify-center items-center cursor-pointer p-4 rounded-xl border-2 border-gray-700">
+                            <img src='{{asset('plan-type-for-life-darkmode.png')}}' alt="Team Use" class="w-[200px] h-auto" />
+                            <div class="font-medium text-lg dark:text-white pt-4">Team</div>
+                        </div>
+                    </label>
+                </div>
 
-        @if($errors->any())
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
+                <button id="continueButton" class="bg-blue-500 rounded-lg text-white py-2 mx-[5rem] transition-all duration-700" type="submit" disabled>
+                    Continue
+                </button>
+
+            </form>
+
+            @if($errors->any())
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
     </div>
-</div>
-</body>
-</html>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const radioButtons = document.querySelectorAll('input[type="radio"]');
+            const continueButton = document.getElementById('continueButton');
+
+            radioButtons.forEach(function(radioButton) {
+                radioButton.addEventListener('change', function() {
+                    continueButton.disabled = false;
+                });
+            });
+        });
+    </script>
+
+    <style>
+        #continueButton:disabled {
+            background-color: #7299b7;
+        }
+    </style>
+
+@endsection

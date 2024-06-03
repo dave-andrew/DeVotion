88a328a17067e7721427ba6ff02af6ad620a5b23
Riccardo <riccardo@binus.ac.id>
2024-06-03 16:31:54 +0700

    fix: header input

--- resources/views/components/inputs/h1-input.blade.php
+++ resources/views/components/inputs/h1-input.blade.php
@@ -1,5 +1,5 @@
-<div class="group w-full flex items-start ">
-    <button class="group-hover:block hidden p-1 hover:bg-gray-100 rounded-md"><i class="fa-solid fa-plus"></i></button>
-    <button class="group-hover:block hidden p-1 hover:bg-gray-100 rounded-md"><i class="fa-solid fa-grip-vertical"></i></button>
-    <textarea class="text-3xl textarea" type="text" placeholder="Untitled"></textarea>
+<div class="group w-full flex items-start -ml-16">
+    <button class="group-hover:opacity-100 opacity-0 px-2 py-0.5 hover:bg-gray-100 rounded-md text-gray-400"><i class="fa-solid fa-plus"></i></button>
+    <button class="group-hover:opacity-100 opacity-0 px-2 py-0.5 hover:bg-gray-100 rounded-md text-gray-400"><i class="fa-solid fa-grip-vertical"></i></button>
+    <textarea class="text-3xl textarea ml-2" type="text" placeholder="Untitled"></textarea>
 </div>

517357149d29b8bf288fceed922c9a5c058f8385
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-06-16 14:37:15 +0700

    feat: invitations

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invitation;
+use App\Models\User;
 use App\Models\Workspaceuser;
 use Illuminate\Http\Request;
 
 class InvitationController extends Controller
 {
+
+    public function create(Request $request)
+    {
+        $workspace_id = $request->workspace_id;
+
+        $user_id = User::where('email', $request->email)->first()->id;
+
+        $invitation = new Invitation();
+        $invitation->workspace_id = $workspace_id;
+        $invitation->user_id = $user_id;
+        $invitation->invited_by = auth()->id();
+        $invitation->save();
+
+        return redirect()->route('workspaces.show', ['workspace' => $workspace_id]);
+    }
 
     public function accept(Request $request)
     {

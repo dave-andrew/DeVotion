15ed5838fa3e5482158e35417b70aa5c7bbd8f6a
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-07-23 18:40:55 +0700

    fix: mixed content error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" href="/Notion_app_logo_D.png" type="image/png">

52610181f7b45244e7401f53669af20049e0581a
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-07-23 23:35:12 +0700

    fix: workspace ui error

--- resources/views/layouts/workspace.blade.php
+++ resources/views/layouts/workspace.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>DeVotion | @yield('title')</title>
     @livewireStyles
 </head>
